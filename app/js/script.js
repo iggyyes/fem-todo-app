3a1ddc47f4599d1b39f7af7b686cc64c199e0df0
iggyyes <igamwisniewska@gmail.com>
2023-04-17 21:24:21 +0100

    refactor todo input + dynamic sizing in js

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,33 @@
 console.log('pieczareczki')
+
+const input = document.getElementById('new-todo');
+const taskInput = document.getElementById('taskDescription1');
+const taskInput2 = document.getElementById('taskDescription2');
+const inputHelper = document.getElementById('input-helper');
+
+const autoSizeInput = (element) => {
+  element.style.height = 'auto';
+  element.style.height = `${element.scrollHeight}px`;
+  inputHelper.textContent = `${element.value.length} characters entered`;
+};
+
+input.addEventListener('input', () => {
+  autoSizeInput(input);
+});
+
+taskInput.addEventListener('input', () => {
+  autoSizeInput(taskInput);
+});
+
+taskInput2.addEventListener('input', () => {
+    autoSizeInput(taskInput2);
+  });
+
+function checkEnter(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.getElementById("task-form").submit();
+    }
+}
+
+
