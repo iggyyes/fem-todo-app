1753e8b90d056f1ab119cf7625216f5787305013
iggyyes <igamwisniewska@gmail.com>
2023-04-24 19:58:44 +0100

    m

--- gulpfile.js
+++ gulpfile.js
@@ -61,15 +61,3 @@ exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
 
 // Build Gulp Task
 exports.build = series(scssTask, jsTask);
-
-//gh pages
-var gulp        = require('gulp');
-var deploy      = require('gulp-gh-pages');
-
-/**
- * Push build to gh-pages
- */
-gulp.task('deploy', function () {
-  return gulp.src("./dist/**/*")
-    .pipe(deploy())
-});
